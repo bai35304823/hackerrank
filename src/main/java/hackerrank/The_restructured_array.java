0af0b49b4f80e1c84cf91f1f5940267fce69a17b
shaopeng <shaopeng@ncs.com.sg>
2022-11-30 14:06:57 +0800

    ----add case, Plus Mult Array/simple array rotation game/Make Median

---
+++ src/main/java/hackerrank/The_restructured_array.java
@@ -0,0 +1,68 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+// all test case pass, score 50/50
+public class The_restructured_array {
+	// Here a generic function to convert from 1D -> 2D array:
+	public static int[][] monoToBidi( final int[] array, final int rows, final int cols ) {
+	    if (array.length != (rows*cols))
+	        throw new IllegalArgumentException("Invalid array length");
+
+	    int[][] bidi = new int[rows][cols];
+	    for ( int i = 0; i < rows; i++ )
+	        System.arraycopy(array, (i*cols), bidi[i], 0, cols);
+
+	    return bidi;
+	}
+	
+	// do the contrary (convert from 2D -> 1D array)
+	public static int[] bidiToMono( final int[][] array ) {
+	    int rows = array.length, cols = array[0].length;
+	    int[] mono = new int[(rows*cols)];
+	    for ( int i = 0; i < rows; i++ )
+	        System.arraycopy(array[i], 0, mono, (i*cols), cols);    
+	        return mono;
+	}
+	
+	public static List<Integer> getElement(List<Integer> arry, List<List<Integer>> query) {
+		// handle null == {arry}
+		arry = Optional.ofNullable(arry)
+				 .orElseGet(Collections::emptyList);
+		query = Optional.ofNullable(query)
+				 .orElseGet(Collections::emptyList);
+		// the 1st element in {arry} represent length of each row of the 2D array	
+		int lengtg = arry.stream().findFirst().get();
+		
+		List<Integer> results = new  ArrayList<>();
+		int[] input = arry
+				 .stream().skip(1)
+				 .mapToInt(Integer::new)
+				 .toArray();
+							 
+				 int[][] twoD_arr = monoToBidi( input, (arry.size() - 1)/lengtg, lengtg );
+				 
+				 query.forEach(s -> {
+					 if (Objects.isNull(s)) {
+						 return;
+					 }
+					 // int query[q][2] : the row and column positions queried
+					 // row location= s.get(0) - 1
+					 // column location = s.get(1) - 1
+					 // 
+					 // eg, [4,1] -> twoD_arr[3][0]
+					 results.add(twoD_arr[s.get(0)-1][s.get(1)-1]);
+				 } );
+		 
+				 return results;	           
+		
+	}
+	
+	  public static final <T> List<T> ofEmptyList(List<T> list) {
+	        return (List<T>) (Optional.ofNullable(list)
+			 .orElseGet(Collections::emptyList));
+	  }
+}
---
+++ src/test/java/com/demo/spring/Application/The_restructured_arrayTests.java
@@ -0,0 +1,5 @@
+package com.demo.spring.Application;
+
+public class The_restructured_arrayTests {
+
+}
