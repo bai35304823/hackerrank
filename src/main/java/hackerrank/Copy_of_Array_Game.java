20709179748662e03ce8196c0f902c7e740ebbcd
shaopeng <shaopeng@ncs.com.sg>
2023-01-16 17:43:26 +0800

    add solution for copy of array game

--- src/main/java/hackerrank/Copy_of_Array_Game.java
+++ src/main/java/hackerrank/Copy_of_Array_Game.java
@@ -1,50 +1,47 @@
 package hackerrank;
-import java.io.*;
-//
-import java.util.Arrays;
+
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 //https://www.geeksforgeeks.org/minimum-number-increment-operations-make-array-elements-equal/
 public class Copy_of_Array_Game {
 
 	// Java program to find min operation
-	
 
+	// Function to print minimum operation required to make
+	// all elements of an array equal
+	static long countMoves(List<Integer> numbers) {
+		long[] arr = numbers.stream().mapToLong(Integer::new).toArray();
+		long arraySum, smallest, arr_size = arr.length;
+		arraySum = 0;
+		smallest = arr[0];
+		for (int i = 0; i < arr_size; i++) {
+			// If current element is smaller than update smallest
+			if (arr[i] < smallest)
+				smallest = arr[i];
 
-		// Function to print minimum operation required to make
-		// all elements of an array equal
-		static void printMinOp(List<Integer> numbers)
-		{
-			 int [] arr = numbers.stream().mapToInt(Integer::new).toArray();
-			int arraySum, smallest, arr_size = arr.length;
-			arraySum = 0;
-			smallest = arr[0];
-			for (int i = 0; i < arr_size; i++) {
-				// If current element is smaller than update smallest
-				if (arr[i] < smallest)
-					smallest = arr[i];
-
-				// find array sum
-				arraySum += arr[i];
-			}
-
-			int minOperation = arraySum - arr_size * smallest;
-
-			// Print min operation required
-			System.out.println("Minimum Operation = " + minOperation);
+			// find array sum
+			arraySum += arr[i];
 		}
 
-		// Driver program to test above functions
-		public static void main(String[] args)
-		{
-			int arr[] = { 5, 6, 2, 4, 3 };
-			printMinOp(Arrays.stream(arr).boxed().collect(Collectors.toList()));
-		}
+		long minOperation = arraySum - arr_size * smallest;
+
+		// Print min operation required
+		return minOperation;
 	}
 
-	// This code is contributed by Sania Kumari Gupta
+	// Driver program to test above functions
+	public static void main(String[] args) {
+		// sample case in example
+		// output: 7
+		System.out.println(countMoves(List.of(3, 4, 6, 6, 3)));
 
+		// sample case 0
+		// output: 7
+		System.out.println(countMoves(List.of(5, 6, 8, 8, 5)));
 
+		// sample case 1
+		// output: 0
+		System.out.println(countMoves(List.of(2, 2, 2)));
+	}
 
+}
--- src/main/java/hackerrank/Tool_List.java
+++ src/main/java/hackerrank/Tool_List.java
@@ -1,31 +1,60 @@
 package hackerrank;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class Tool_List {
-	public int toolChanger(String[] tools, int startIndex, String target) {
+
+	public static int toolChange(String[] tools, int startIndex, String target) {
 		if (tools[startIndex].equals(target)) {
-		return 0;
+			return 0;
 		}
 		int left = startIndex - 1, right = startIndex + 1, steps = 1;
 		if (left < 0) {
-		left = tools.length - 1;
+			left = tools.length - 1;
 		}
 		if (right > tools.length - 1) {
-		right = 0;
+			right = 0;
 		}
 		while (left >= 0 && left < tools.length && right >= 0 && right < tools.length) {
-		if (tools[left].equals(target) || tools[right].equals(target)) {
-		break;
-		}
-		left--;
-		if (left < 0) {
-		left = tools.length - 1;
-		}
-		right++;
-		if (right > tools.length - 1) {
-		right = 0;
-		}
-		steps++;
+			if (tools[left].equals(target) || tools[right].equals(target)) {
+				break;
+			}
+			left--;
+			if (left < 0) {
+				left = tools.length - 1;
+			}
+			right++;
+			if (right > tools.length - 1) {
+				right = 0;
+			}
+			steps++;
 		}
 		return steps;
-		}
+	}
+
+	public static void main(String[] args) {
+
+		// Sample case in question
+		// output : 1
+		List<String> s = List.of("ballendmill", "keywaycutter", "slotdrill", "facemill");
+		System.out.println(toolChanger(s, 1, "ballendmill"));
+
+		// Sample case 0
+		// output : 2
+		System.out
+				.println(toolChanger(List.of("ballendmill", "facemill", "keywaycutter", "slotdrill"), 1, "slotdrill"));
+		// Sample case 1
+		// output : 1
+		System.out
+				.println(toolChanger(List.of("facemill", "slotdrill", "ballendmill", "ballendmill"), 0, "ballendmill"));
+
+	}
+
+	public static int toolChanger(List<String> tools, int startIndex, String target) {
+		return toolChange(Optional.ofNullable(tools).orElseGet(Collections::emptyList).stream().toArray(String[]::new),
+				startIndex, target);
+	}
+
 }
