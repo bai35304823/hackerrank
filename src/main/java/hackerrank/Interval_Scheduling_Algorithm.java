05560ffa1a938bec21ab2432d23bc9e0cfec7dc0
shaopeng <shaopeng@ncs.com.sg>
2023-01-16 17:12:55 +0800

    add solution for University Career Fair

--- src/main/java/hackerrank/Interval_Scheduling_Algorithm.java
+++ src/main/java/hackerrank/Interval_Scheduling_Algorithm.java
@@ -1,45 +1,60 @@
 package hackerrank;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 public class Interval_Scheduling_Algorithm {
 //https://leetcode.com/discuss/interview-question/374846/Twitter-or-OA-2019-or-University-Career-Fair
-	//https://zhuanlan.zhihu.com/p/91254104
-	 public static int num_maxEvents (int[] arr, int[]dur) {
-	        int drop = 0;
-	        int NumOfEvents = arr.length;
-	        int [][] intervals = new int[NumOfEvents][2];
+	// https://zhuanlan.zhihu.com/p/91254104
+	public static int maxEvents(List<Integer> arrival, List<Integer> duration) {
+		// Write your code here
+		int[] arr = Optional.ofNullable(arrival).orElseGet(Collections::emptyList).stream().mapToInt(i -> i).toArray();
+		int[] dur = Optional.ofNullable(duration).orElseGet(Collections::emptyList).stream().mapToInt(i -> i).toArray();
+		return num_maxEvents(arr, dur);
+	}
 
-	        for (int i = 0; i < NumOfEvents; i++) {
-	            intervals[i] = new int[] {arr[i], arr[i] + dur[i]};
-	        }
+	public static int num_maxEvents(int[] arr, int[] dur) {
+		int drop = 0;
+		int NumOfEvents = arr.length;
+		int[][] intervals = new int[NumOfEvents][2];
 
-	        Arrays.sort(intervals, (a, b) ->(a[1] - b[1]));
-	        // the finish time of first event;
-	        int curTime = intervals[0][1];
+		for (int i = 0; i < NumOfEvents; i++) {
+			intervals[i] = new int[] { arr[i], arr[i] + dur[i] };
+		}
 
-	        for (int i = 1; i < NumOfEvents; i++) {
-	            int [] toSchedual = intervals[i];
-	            if (toSchedual[0] < curTime)
-	                drop++;
-	            else {
-	                curTime = toSchedual[1];
-	            }
-	        }
-	        return NumOfEvents - drop;
+		Arrays.sort(intervals, (a, b) -> (a[1] - b[1]));
+		// the finish time of first event;
+		int curTime = intervals[0][1];
 
+		for (int i = 1; i < NumOfEvents; i++) {
+			int[] toSchedual = intervals[i];
+			if (toSchedual[0] < curTime)
+				drop++;
+			else {
+				curTime = toSchedual[1];
+			}
+		}
+		return NumOfEvents - drop;
 
-	    }
+	}
 
-	    public static void main(String[] args) {
-	        int[] arrival1 = {1,3,3,5,7};
-	        int[] duration1 = {2,2,1,2,1};
-	        System.out.println(num_maxEvents(arrival1, duration1));
-	        int[] arrival2 = {1,3,3,5,7, 6, 9};
-	        int[] duration2 = {2,2,1,2,1, 10, 2};
-	        System.out.println(num_maxEvents(arrival2, duration2));
-	        int[] arrival = {1,3,35,7};
-	        int[] duration = {2,2,1,2,1};
-	        System.out.println(num_maxEvents(arrival, duration));
-	    }
+	public static void main(String[] args) {
+		// sample case in question
+		// output : 4
+		int[] arrival1 = { 1, 3, 3, 5, 7 };
+		int[] duration1 = { 2, 2, 1, 2, 1 };
+		System.out.println(num_maxEvents(arrival1, duration1));
+		// sample case 0
+		// output : 3
+		int[] arrival2 = { 1, 3, 5 };
+		int[] duration2 = { 2, 2, 2 };
+		System.out.println(num_maxEvents(arrival2, duration2));
+		// sample case 1
+		// output : 1
+		int[] arrival = { 1 };
+		int[] duration = { 5 };
+		System.out.println(num_maxEvents(arrival, duration));
+	}
 }
--- src/main/java/hackerrank/closest_numbers.java
+++ src/main/java/hackerrank/closest_numbers.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class closest_numbers {
-	 public static List<Integer> closestNumbers(List<Integer> arr) {
+	 public static void closestNumbers(List<Integer> arr) {
 	        arr.sort((a,b) -> a.compareTo(b));
 	        int minDiff = Integer.MAX_VALUE;
 	        for(int i=1;i<arr.size();i++) minDiff = Math.min(minDiff,  arr.get(i) - arr.get(i-1));
@@ -12,25 +12,27 @@ public class closest_numbers {
 	        for(int i=1;i<arr.size();i++) if(arr.get(i) - arr.get(i-1) == minDiff) {
 	            res.add(arr.get(i-1));
 	            res.add(arr.get(i));
+	            System.out.print(arr.get(i-1) + " ");
+	            System.out.println(arr.get(i));
 	        } 
-	        return res;
+	       
 	    }
 	 
 	 public static void main(String[] args) {
 		// Sample case in question
 		// output : 2 4
 		//			4 6
-		System.out.println(closestNumbers(new ArrayList<Integer>(List.of(6,2,4,10))));
+		closestNumbers(new ArrayList<Integer>(List.of(6,2,4,10)));
 
 		// Sample case 0
 		// output : 1 2
 		//			2 3
 		//			3 4
-		System.out.println(closestNumbers(new ArrayList<Integer>(List.of(4,2,1,3))));
+		closestNumbers(new ArrayList<Integer>(List.of(4,2,1,3)));
 		
 		// Sample case 1
 		// output : -2 -1
 		//		    3 4
-		System.out.println(closestNumbers(new ArrayList<Integer>(List.of(4,-2,-1,3))));
+		closestNumbers(new ArrayList<Integer>(List.of(4,-2,-1,3)));
 	}
 }
