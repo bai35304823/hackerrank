f29b723016dd65fc3bf8e1be05ed99b256cbade5
shaopeng <shaopeng@ncs.com.sg>
2023-02-15 17:04:58 +0800

    remove eureka client config

--- src/main/java/com/demo/spring/Application/DemoApplication.java
+++ src/main/java/com/demo/spring/Application/DemoApplication.java
@@ -1,15 +1,7 @@
 package com.demo.spring.Application;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
