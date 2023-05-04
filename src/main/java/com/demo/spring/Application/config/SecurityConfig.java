b41bb8a1259ceca69b60d196442f157b9bd65d9e
shaopeng <shaopeng@ncs.com.sg>
2023-05-04 18:25:28 +0800

    add spring security

--- src/main/java/com/demo/spring/Application/config/SecurityConfig.java
+++ src/main/java/com/demo/spring/Application/config/SecurityConfig.java
@@ -1,29 +1,70 @@
-package com.demo.spring.Application.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		 http.csrf().disable(); //Close CSRF verification
-		http.formLogin().defaultSuccessUrl("/getAll");
-		 return http.build();
-		/*
-		 * http.csrf().disable(); //Close CSRF verification
-		 * http.authorizeRequests().anyRequest().authenticated(); //Intercept all
-		 * interfaces http. formLogin() // .loginPage("/login") //Custom Login page //
-		 * .usernameParameter("myUsername") //Custom username parameter name //
-		 * .passwordParameter("myPassword") //Custom password parameter name //
-		 * .loginProcessingUrl("/doLogin") //The transfer address after the defined form
-		 * is submitted .defaultSuccessUrl("/success") //Address to jump after
-		 * successful verification // .failureUrl("/failure") //The address of the
-		 * verification failure jump .permitAll(); //Interfaces related to form login
-		 * are not intercepted
-		 * 
-		 */    }
-}
-
+  package com.demo.spring.Application.config;
+  
+  import org.springframework.beans.factory.annotation.Autowired; import
+  org.springframework.context.annotation.Bean; import
+  org.springframework.context.annotation.Configuration; import
+  org.springframework.security.authentication.AuthenticationManager; import
+  org.springframework.security.config.BeanIds; import
+  org.springframework.security.config.annotation.authentication.builders.
+  AuthenticationManagerBuilder; import
+  org.springframework.security.config.annotation.authentication.configuration.
+  AuthenticationConfiguration; import
+  org.springframework.security.config.annotation.web.builders.HttpSecurity;
+  import
+  org.springframework.security.config.annotation.web.builders.WebSecurity;
+  import org.springframework.security.config.annotation.web.configuration.
+  EnableWebSecurity; import
+  org.springframework.security.config.annotation.web.configuration.
+  WebSecurityConfigurerAdapter; import
+  org.springframework.security.config.http.SessionCreationPolicy; import
+  org.springframework.security.crypto.password.NoOpPasswordEncoder; import
+  org.springframework.security.crypto.password.PasswordEncoder; import
+  org.springframework.security.web.SecurityFilterChain; import
+  org.springframework.security.web.authentication.
+  UsernamePasswordAuthenticationFilter; import
+  org.springframework.web.servlet.config.annotation.CorsRegistry; import
+  org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+  
+  import com.demo.spring.Application.filter.JwtFilter; import
+  com.demo.spring.Application.service.CustomUserDetailsService;
+  
+  @Configuration
+  
+  @EnableWebSecurity public class SecurityConfig {
+  
+  @Autowired private CustomUserDetailsService userDetailsService;
+  
+  @Autowired private JwtFilter jwtFilter;
+  
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+      return NoOpPasswordEncoder.getInstance();
+  }
+  @Bean public AuthenticationManager
+  authenticationManager(AuthenticationConfiguration
+  authenticationConfiguration) throws Exception { return
+  authenticationConfiguration.getAuthenticationManager(); }
+  
+  @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
+  Exception {
+  
+  
+  AuthenticationManagerBuilder authenticationManagerBuilder =
+  http.getSharedObject(AuthenticationManagerBuilder.class);
+  authenticationManagerBuilder.userDetailsService(userDetailsService);
+  AuthenticationManager authenticationManager =
+  authenticationManagerBuilder.build();
+  
+  http.csrf().disable().cors().disable() .authorizeHttpRequests()
+  .antMatchers("/authenticate", "/api/v2/registrations/**").permitAll()
+  .anyRequest().authenticated() .and()
+  .authenticationManager(authenticationManager)
+  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+  
+  return http.build(); }
+  
+  
+  public void configure(WebSecurity web) throws Exception { web.ignoring()
+  .antMatchers("/api/v2/registrations/"); } }
